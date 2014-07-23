f29a0c486814108f68f9e095e10ba248f7a9d01f
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2014-07-23 14:59:00 +0200

    Variable assignement are highlighted when mouse is on variable use.

--- learning.py
+++ learning.py
@@ -23,6 +23,10 @@ class LineTagger:
             return sub_executor(node)
 
     def tag_Assignement(self, node):
+        tag_name = "%s_assign"%(node.name.v)
+        start_index = "%d.%d"%(self.lineno, node.name.start)
+        end_index = "%d.%d"%(self.lineno, node.name.end)
+        self.textWidget.tag_add(tag_name, start_index, end_index)
         self.tag(node.value)
 
     def tag_Call(self, node):
@@ -32,14 +36,12 @@ class LineTagger:
             self.context.append(getattr(functions, function_name).arguments[index])
             self.tag_argument(function_name, index, argument)
             self.context.pop()
-        
 
     def tag_functionIdentifier(self, node):
         tag_name = "%s_%d_call"%(node.v, self.lineno)
         start_index = "%d.%d"%(self.lineno, node.start)
         end_index = "%d.%d"%(self.lineno, node.end)
         self.textWidget.tag_add(tag_name, start_index, end_index)
-        self.textWidget.tag_configure(tag_name, foreground="red")
         self.textWidget.tag_bind(tag_name, "<Enter>", lambda e: context.helpv.set(getattr(functions,node.v).help))
 
     def tag_argument(self, function_name, index, node):
@@ -62,10 +64,21 @@ class LineTagger:
         self.textWidget.tag_add(tag_name, start_index, end_index)
         self.textWidget.tag_bind(tag_name, "<Enter>", on_enter)
         self.textWidget.tag_bind(tag_name, "<Leave>", on_leave)
-        self.textWidget.tag_configure(tag_name, background="#CCFFCC")
         self.directChild = True
         self.tag(node)
         self.directChild = False
+
+    def tag_Identifier(self, node):
+        def on_enter(event):
+            context.text.tag_configure("%s_assign"%node.v, foreground="red")
+        def on_leave(event):
+            context.text.tag_configure("%s_assign"%node.v, foreground="")
+        tag_name = "%s_%d"%(node.v, self.lineno)
+        start_index = "%d.%d"%(self.lineno, node.start)
+        end_index = "%d.%d"%(self.lineno, node.end)
+        self.textWidget.tag_bind(tag_name, "<Enter>", on_enter)
+        self.textWidget.tag_bind(tag_name, "<Leave>", on_leave)
+        self.textWidget.tag_add(tag_name, start_index, end_index)
 
     def tag_Paren(self, node):
         self.tag(node.v)
