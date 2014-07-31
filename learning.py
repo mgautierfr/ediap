cab9f9a89ba3c8a8a8577a5e5f25c12285d7324f
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2014-07-31 17:41:57 +0200

    update default source.

--- learning.py
+++ learning.py
@@ -8,23 +8,30 @@ import interpreter
 import gui
 from program import Program, Line
 
-default_source = """function test(a, b)
+default_source = """function house(x, y, w, h)
+  ratio = 0.3
+  triangle(x,y+h*ratio, x+w,y+h*ratio, x+w/2,y)
+  rectangle(x, y+h*ratio, w, h*(1-ratio))
+
+function test(a, b)
   fill(a*10, b, 100)
-  ellipse(x*a, b, 100, 100)
+  ellipse(x*a, b, 10, 10)
   x = 5
-  ellipse(x*a, b, 100, 100)
+  ellipse(x*a, b, 10, 10)
 x = 11
-view(0, 0, 1000, 1000)
+view(0, 0, 100, 100)
 fill(0, 0, 255)
-ellipse(10, 10, 100, 100)
-rectangle(200, 200+x*9, 150, 50)
+ellipse(1, 1, 10, 10)
+rectangle(20, 20+x*9, 15, 5)
 fill(255, 255, 0)
 if x == 11
   fill(0, 0, 255)
-test(10, 200)
+test(1, 20)
+house(10, 10, 10, 20)
+quad(10,10, 10,20, 20,20, 20,10)
 x = 0
 while x < 3
-  rectangle(500+110*x, 100+10*x, 100, 100)
+  rectangle(50+11*x, 10+1*x, 10, 10)
   x = x + 1
 x=0
 while x < 5
@@ -32,7 +39,7 @@ while x < 5
   test(5, 5)
   while y < 5
     fill(25*x, 25*y, 0)
-    rectangle(500+50*x, 500+50*y, 30, 30)
+    rectangle(50+5*x, 50+5*y, 3, 3)
     y = y + 1
   x = x + 1"""
 
